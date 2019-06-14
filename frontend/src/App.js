549c243183a2e5accf8db4ba5e674f0be9dd9a8d
JeanCHilger <hilgerjeancarlo@gmail.com>
2019-06-14 00:55:06 -0300

    adds layout to frontend application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>AAAAA</h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes />
+        </BrowserRouter>  
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import api from '../services/api';
+
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
+class Feed extends Component {
+	state = {
+		feed: [],	
+	};
+	
+	async componentDidMount() {
+		const response = await api.get('posts');
+		
+		this.setState({ feed: response.data });
+	}	
+	
+	render() {
+		return (
+			<section id="post-list">
+				{ this.state.feed.map(post => (
+				<article>
+					<header>
+						<div className="user-info">
+							<span> { post.author } </span>
+							<span> { post.place } </span>
+						</div>
+						
+						<img src={ more } alt="Mais" />
+					</header>
+					
+					<img src={ `http://localhost:3333/files/${post.image}` } alt="merdaaa" />
+					
+					<footer>
+						<div className="actions">
+							<img src={ like } alt="Like" />
+							<img src={ comment } alt="Comment" />
+							<img src={ send } alt="Send" />
+						</div>
+						
+						<strong> { post.likes } </strong>
+						
+						<p>
+							{ post.description }
+							
+							<span> post.hashtags </span>
+						</p>
+						
+					</footer>
+					
+				</article>
+			)) }
+			</section>
+		);
+	}
+}
+
+export default Feed;
